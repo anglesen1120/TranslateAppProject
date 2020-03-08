5941ab85848fc67a8b2b9fce3f389a03af0cea71
Muhammad Ali Nizami <kustalics@gmail.com>
2020-03-09 00:15:01 +0500

    DSFD

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,7 +10,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
+      appBar: AppBar(title: Text('Home Page bAR')),
       body: ListView(
         children: <Widget>[
           TextFormField(
